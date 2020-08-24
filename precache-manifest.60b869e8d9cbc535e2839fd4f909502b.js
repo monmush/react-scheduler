f69b7c6c32be0f5acd7fa353137be02a3b119c8e
Minh Pham <omiph001@gmail.com>
2020-08-24 11:26:19 +0700

    Updates

---
+++ precache-manifest.60b869e8d9cbc535e2839fd4f909502b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29acaf18b4404b7da5af45171476e64",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fbd74ebb5bc7b816914d",
+    "url": "./static/css/2.3c165aea.chunk.css"
+  },
+  {
+    "revision": "72301b5d59e1a9c09be3",
+    "url": "./static/css/main.ed0437db.chunk.css"
+  },
+  {
+    "revision": "fbd74ebb5bc7b816914d",
+    "url": "./static/js/2.1a15f408.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.1a15f408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72301b5d59e1a9c09be3",
+    "url": "./static/js/main.5dc7f8bc.chunk.js"
+  },
+  {
+    "revision": "9c8e25a8bdb5b74a9960",
+    "url": "./static/js/runtime-main.ad4e31da.js"
+  }
+]);
